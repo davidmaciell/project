5cfd9cc26bf229369de46f48067114e7924f7b00
David Maciel <david.maciel94@gmail.com>
2020-09-16 13:59:49 -0300

    d

--- project/src/index.js
+++ project/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nav from './nav'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-      
-    <App /> 
+      <div>
+        <App /> 
+        <Nav/>
+      </div>
     
   ,document.getElementById('root')
 );
